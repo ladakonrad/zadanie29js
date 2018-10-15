cc9ded00c7f237136771dc502183c0d6b8a1bd6f
Konrad <ladakonrad@gmail.com>
2018-10-15 21:33:00 +0200

    dodano zadanie29js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function twoNumbers(num1, num2) {
+    let suma = num1 + num2;
+   if(num1 == 20 || num2 == 20 || suma<= 20) {
+    console.log(true); } else {
+        console.log(false);
+    }
+}
+
+twoNumbers(21, 8);
